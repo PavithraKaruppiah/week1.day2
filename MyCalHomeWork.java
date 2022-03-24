62283d2de985df8be2a9a03da9ca91b63ff10fb8
PavithraKaruppiah <101961073+PavithraKaruppiah@users.noreply.github.com>
2022-03-24 07:00:55 +0300

    Add files via upload

---
+++ MyCalHomeWork.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class MyCalHomeWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CalculatorHomeWork Calu = new CalculatorHomeWork();
+		System.out.println(Calu.add(5, 6, 7));
+		System.out.println(Calu.sub(10, 5));
+		System.out.println(Calu.mul(4.8, 1.2));
+		System.out.println(Calu.div(8.5f, 4.0f));
+		
+
+	}
+
+}
